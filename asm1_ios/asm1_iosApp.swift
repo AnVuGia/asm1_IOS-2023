e651c34550792e0f3f659ca219e32a209ef86b3f
An Vu <s3926888@rmit.edu.vn>
2023-07-28 10:49:32 +0700

    Initial Commit

---
+++ asm1_ios/asm1_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  asm1_iosApp.swift
+//  asm1_ios
+//
+//  Created by An Vu Gia on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct asm1_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
