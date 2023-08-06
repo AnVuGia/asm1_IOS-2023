54df4fe93871717507b7536f672f4b715533d30e
An Vu <104729655+AnVuGia@users.noreply.github.com>
2023-08-06 19:33:56 +0700

    Update locationRow.swift

--- asm1_ios/Model/locationRow.swift
+++ asm1_ios/Model/locationRow.swift
@@ -1,9 +1,14 @@
-//
-//  locationRow.swift
-//  asm1_ios
-//
-//  Created by An Vu Gia on 28/07/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 1
+  Author: Vu Gia An
+  ID: s3926888
+  Created  date: 06/08/2023
+  Last modified: 06/08/2023
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 
 import SwiftUI
 
